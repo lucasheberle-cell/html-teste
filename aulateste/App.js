fbe868f9150dc4b3a793dee1413288ac14ce782d
LUCAS GUILHERME HEBERLE <lucas.heberle@acad.univates>
2026-08-07 21:46:56 -0300

    novas atualizaçoes

--- aulateste/App.js
+++ aulateste/App.js
@@ -1,13 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable, Button, TouchableHighlight } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>batata</Text>
+      <Pressable >
+  <Text>Babuino leitero Americano</Text>
+</Pressable>
       <StatusBar style="auto" />
+      <Button
+  title="Clique para o biscoito"
+  color="#841584"
+  accessibilityLabel="Learn more about this purple button"
+/>
+<TouchableHighlight
+  activeOpacity={0.6}
+  underlayColor="#c71212"
+>
+  <Text>Olá :D</Text>
+</TouchableHighlight>
     </View>
-  );
+  ); 
 }
 
 const styles = StyleSheet.create({
